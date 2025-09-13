efef9dcb0ce90a8cd07b1ae6c90f9ff093940a16
iran <iranilsonhonorato88@gmail.com>
2025-09-13 18:38:43 -0300

    feat(contaCorrente): Implementacao da classe ContaCorrente

--- src/main/java/com/banksystem/aps2/contaCorrente/ContaCorrente.java
+++ src/main/java/com/banksystem/aps2/contaCorrente/ContaCorrente.java
@@ -1,4 +1,27 @@
 package com.banksystem.aps2.contaCorrente;
 
+import java.time.LocalDate;
+
 public class ContaCorrente {
+    private String agencia;
+    private String conta;
+    private Float salario;
+    private Float limite;
+
+    public ContaCorrente(String agencia, String conta, Float salario, Float limite) {
+        this.agencia = agencia;
+        this.conta = conta;
+        this.salario = salario;
+        this.limite = limite;
+    }
+
+    // Métodos get
+    public String getAgencia() {return agencia;}
+    public String getConta() {return conta;}
+
+
+    // Métodos set
+    public void setAgencia(String agencia) {this.agencia = agencia;}
+    public void setConta(String conta) {this.conta = conta;}
+
 }
