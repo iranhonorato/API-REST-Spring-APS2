a69c85f66b6de0dd1626a5b4d7b0f7d1c79717f4
iran <iranilsonhonorato88@gmail.com>
2025-09-13 13:34:57 -0300

    feat(cliente): Implementando métodos get e set na classe Cliente

--- src/main/java/com/banksystem/aps2/cartoes/Cartoes.java
+++ src/main/java/com/banksystem/aps2/cartoes/Cartoes.java
@@ -1,4 +1,33 @@
 package com.banksystem.aps2.cartoes;
+import java.time.LocalDate;
+
+
 
 public class Cartoes {
+    enum CartaoStatus {ATIVO, CANCELADO}
+    private String numeroCartao;
+    private String tipo;
+    private LocalDate validade;
+    private CartaoStatus status;
+
+    public Cartoes(String numeroCartao, String tipo, LocalDate validade, CartaoStatus status) {
+        this.numeroCartao = numeroCartao;
+        this.tipo = tipo;
+        this.validade = validade;
+        this.status = status;
+    }
+
+
+    // Métodos get
+    public String getNumeroCartao() {return this.numeroCartao;}
+    public String getTipo() {return this.tipo;}
+    public LocalDate getValidade() {return this.validade;}
+    public CartaoStatus getStatus() {return this.status;}
+
+    // Métodos set
+    public void setNumeroCartao(String numeroCartao) {this.numeroCartao = numeroCartao;}
+    public void setTipo(String tipo) {this.tipo = tipo;}
+    public void setValidade(LocalDate validade) {this.validade = validade;}
+    public void setStatus(CartaoStatus status) {this.status = status;}
+
 }
--- src/main/java/com/banksystem/aps2/cliente/Cliente.java
+++ src/main/java/com/banksystem/aps2/cliente/Cliente.java
@@ -1,4 +1,30 @@
 package com.banksystem.aps2.cliente;
+import java.time.LocalDate;
 
 public class Cliente {
+    private String nome;
+    private String cpf;
+    private LocalDate dataNascimento;
+    private Float salario;
+
+    public Cliente(String nome, String cpf, LocalDate dataNascimento, Float salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.salario = salario;
+    }
+
+    // Métodos get
+    public String getNome() {return this.nome;}
+    public String getCpf() {return this.cpf;}
+    public LocalDate getDataNascimento() {return this.dataNascimento;}
+    public Float getSalario() {return this.salario;}
+
+
+    // Métodos set
+    public void setNome(String nome) {this.nome = nome;}
+    public void setCpf(String cpf) {this.cpf = cpf;}
+    public void setDataNascimento(LocalDate dataNascimento) {this.dataNascimento = dataNascimento;}
+    public void setSalario(Float salario) {this.salario = salario;}
+
 }
