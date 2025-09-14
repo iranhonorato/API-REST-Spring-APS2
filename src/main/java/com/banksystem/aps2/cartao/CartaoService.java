b6e351fbc224f280ad15f48aed27f333d39cf934
iran <iranilsonhonorato88@gmail.com>
2025-09-14 17:39:32 -0300

    fix(cartao): Corrigindo código de CartaoService

--- src/main/java/com/banksystem/aps2/cartao/CartaoService.java
+++ src/main/java/com/banksystem/aps2/cartao/CartaoService.java
@@ -18,19 +18,15 @@ import java.util.UUID;
 @Service
 public class CartaoService {
     // Banco de dados em memória
-    private final Map<UUID, Cartao> cartoes = new HashMap<>();
+    private final Map<String, Cartao> cartoes = new HashMap<>();
 
     public Collection<Cartao> listarCartoes() {
         return cartoes.values();
     }
 
     public Cartao buscarPorNumero(String numeroCartao) {
-        return cartoes.values().stream()
-                .filter(c -> c.getNumeroCartao().equals(numeroCartao))
-                .findFirst()
-                .orElse(null);
+        return cartoes.get(numeroCartao);
     }
-
 
     public Cartao salvarCartao(Cartao cartao) {
         if (cartao.getNumeroCartao() == null || cartao.getNumeroCartao().isEmpty()) {
@@ -41,23 +37,14 @@ public class CartaoService {
             throw new IllegalArgumentException("Já existe um cartão com esse número.");
         }
 
-        UUID id = UUID.randomUUID();
-        cartoes.put(id, cartao);
+        cartoes.put(cartao.getNumeroCartao(), cartao);
         return cartao;
     }
 
 
     public void deletar(String numeroCartao) {
+        cartoes.remove(numeroCartao);
 
-        UUID idParaRemover = cartoes.entrySet().stream()
-                .filter(entry -> entry.getValue().getNumeroCartao().equals(numeroCartao))
-                .map(Map.Entry::getKey)
-                .findFirst()
-                .orElse(null);
-
-        if (idParaRemover != null) {
-            cartoes.remove(idParaRemover);
-        }
     }
 
 }
