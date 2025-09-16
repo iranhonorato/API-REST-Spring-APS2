b2c038b14805a34d1ec7fe1bd7379eacd600d00b
iran <iranilsonhonorato88@gmail.com>
2025-09-16 14:20:15 -0300

    feat(conta): Inicializando ContaCorrenteController

--- src/main/java/com/banksystem/aps2/contaCorrente/ContaCorrenteController.java
+++ src/main/java/com/banksystem/aps2/contaCorrente/ContaCorrenteController.java
@@ -1,4 +1,24 @@
 package com.banksystem.aps2.contaCorrente;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/conta")
 public class ContaCorrenteController {
+
+    @Autowired
+    private ContaCorrenteService contaCorrenteService;
+
+    @GetMapping
+    public Collection<ContaCorrente> listarContas(){ return  contaCorrenteService.listarContas(); }
+
+
+    GetMapping
+
 }
